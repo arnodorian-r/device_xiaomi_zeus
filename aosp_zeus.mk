4dc49e72ccb214f7858f556ab30bbfe01ebed6c3
arnodorian-r <geekroy98@gmail.com>
2023-07-10 10:47:58 +0530

    zeus: Bringup for Pixel Experience Based Roms

--- aosp_zeus.mk
+++ aosp_zeus.mk
@@ -9,9 +9,19 @@ $(call inherit-product, device/xiaomi/zeus/device.mk)
 
 # Inherit from common lineage configuration
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zeus
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+PEX_BUILD_TYPE := UNOFFICIAL
+
+# Product Info
+PRODUCT_NAME := aosp_zeus
 PRODUCT_DEVICE := zeus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
